03baf4385cbeedf7ca4303cc7c6f259b5fcc2238
Prashanth N <imprash4u@gmail.com>
2025-05-18 18:23:42 +0530

    Added Service class implementation and exceptions with global exception handler

--- src/main/java/com/p8io/order_processing_system/dto/OrderRequestDTO.java
+++ src/main/java/com/p8io/order_processing_system/dto/OrderRequestDTO.java
@@ -4,9 +4,12 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
 public class OrderRequestDTO {
+    @NotNull(message = "Required!, Order id must not be null")
     private Long orderId;
 
     @NotBlank(message = "Item name must not be blank")
@@ -16,4 +19,30 @@ public class OrderRequestDTO {
     @Min(value = 1, message = "Quantity must be at least 1")
     private Integer quantity;
 
+    public @NotNull(message = "Required!, Order id must not be null") Long getOrderId() {
+        return orderId;
+    }
+
+    public OrderRequestDTO setOrderId(@NotNull(message = "Required!, Order id must not be null") Long orderId) {
+        this.orderId = orderId;
+        return this;
+    }
+
+    public @NotBlank(message = "Item name must not be blank") String getItem() {
+        return item;
+    }
+
+    public OrderRequestDTO setItem(@NotBlank(message = "Item name must not be blank") String item) {
+        this.item = item;
+        return this;
+    }
+
+    public @NotNull(message = "Quantity is required") @Min(value = 1, message = "Quantity must be at least 1") Integer getQuantity() {
+        return quantity;
+    }
+
+    public OrderRequestDTO setQuantity(@NotNull(message = "Quantity is required") @Min(value = 1, message = "Quantity must be at least 1") Integer quantity) {
+        this.quantity = quantity;
+        return this;
+    }
 }
--- src/main/java/com/p8io/order_processing_system/entity/Order.java
+++ src/main/java/com/p8io/order_processing_system/entity/Order.java
@@ -9,13 +9,39 @@ import lombok.NoArgsConstructor;
 @Table(name = "orders")
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
 public class Order {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
     private Long orderId;
     @Column(nullable = false)
     private String item;
     @Column(nullable = false)
     private int quantity;
+
+    public String getItem() {
+        return item;
+    }
+
+    public Order setItem(String item) {
+        this.item = item;
+        return this;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Order setQuantity(int quantity) {
+        this.quantity = quantity;
+        return this;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public Order setOrderId(Long orderId) {
+        this.orderId = orderId;
+        return this;
+    }
 }
