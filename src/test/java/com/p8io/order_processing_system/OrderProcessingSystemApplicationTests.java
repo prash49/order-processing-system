08a9d471c83493b8d128a98eb7f15df3d824a0bf
Prashanth N <imprash4u@gmail.com>
2025-05-18 12:43:06 +0530

    Initial Order Process Project Setup

---
+++ src/test/java/com/p8io/order_processing_system/OrderProcessingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p8io.order_processing_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProcessingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
