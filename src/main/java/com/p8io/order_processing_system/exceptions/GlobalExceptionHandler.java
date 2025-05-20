d321e1b8e3736e0657ecd9baa6d0a3ec6bfe88d2
Prashanth N <imprash4u@gmail.com>
2025-05-20 07:50:35 +0530

    Refactored Code for Error response Handling

--- src/main/java/com/p8io/order_processing_system/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/p8io/order_processing_system/exceptions/GlobalExceptionHandler.java
@@ -2,36 +2,45 @@ package com.p8io.order_processing_system.exceptions;
 
 import com.p8io.order_processing_system.dto.ApiResponse;
 import com.p8io.order_processing_system.util.ApplicationConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(OrderAlreadyExistsException.class)
-    public ResponseEntity<ApiResponse<Object>> handleOrderAlreadyExistsExcepton(OrderAlreadyExistsException orderAlreadyExistsException) {
-        ApiResponse<Object> response = new ApiResponse<>(ApplicationConstants.FAILURE, HttpStatus.BAD_REQUEST.value(), orderAlreadyExistsException.getMessage());
+    public ResponseEntity<ApiResponse<Object>> handleOrderAlreadyExistsException(OrderAlreadyExistsException ex) {
+        log.info("handle OrderAlready exists exception");
+        ApiResponse<Object> response = new ApiResponse<>(
+                ApplicationConstants.FAILURE,
+                HttpStatus.BAD_REQUEST.value(),
+                ex.getMessage()
+        );
+        log.info(response.toString());
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiResponse<Map<String, String>>> handleValidationExceptions(MethodArgumentNotValidException exception) {
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
-        exception.getBindingResult().getAllErrors()
-                .forEach(error -> {
-                            String filedName = (error instanceof FieldError) ? ((FieldError) error).getField() : error.getObjectName();
-                            String errorMessage = error.getDefaultMessage();
-                            errors.put(filedName, errorMessage);
-                        }
-                );
-        ApiResponse<Map<String, String>> response = new ApiResponse<>(ApplicationConstants.ERROR, HttpStatus.BAD_REQUEST.value(), errors.toString());
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Object>> handleGenericException(Exception ex) {
+        ApiResponse<Object> response = new ApiResponse<>(ApplicationConstants.FAILURE, HttpStatus.INTERNAL_SERVER_ERROR.value(), "Unexpected error: " + ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
