0b17c9d868947956c7e8fe6ad4e689826719a051
Prashanth N <imprash4u@gmail.com>
2025-05-21 00:54:54 +0530

    Added Test cases for OrderService Impl post method which is for create orders

--- src/test/java/com/p8io/order_processing_system/service/OrderServiceImplTest.java
+++ src/test/java/com/p8io/order_processing_system/service/OrderServiceImplTest.java
@@ -1,28 +1,54 @@
 package com.p8io.order_processing_system.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.p8io.order_processing_system.dto.OrderRequestDTO;
 import com.p8io.order_processing_system.entity.Order;
+import com.p8io.order_processing_system.exceptions.OrderAlreadyExistsException;
+import com.p8io.order_processing_system.messaging.KafkaProducer;
 import com.p8io.order_processing_system.repository.OrderRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.MessageSource;
+import org.springframework.test.util.ReflectionTestUtils;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
+import java.util.concurrent.ExecutionException;
 
 @ExtendWith(MockitoExtension.class)
 public class OrderServiceImplTest {
 
     @Mock
-   private OrderRepository orderRepository;
+    private OrderRepository orderRepository;
+
+    @Mock
+    private KafkaProducer kafkaProducer;
+
+    @Mock
+    MessageSource messageSource;
+
+    private String orderPushTopic = "test-order-queue";
+
+    private ObjectMapper testObjectMapper = new ObjectMapper();
 
     @InjectMocks
     private OrderServiceImpl orderService;
+
+    @BeforeEach
+    void setUp() {
+        ReflectionTestUtils.setField(orderService, "orderPushTopic", orderPushTopic);
+    }
 
     @Test
     void getOrderById_shouldReturnOrderInOptionalSuccess() {
@@ -39,8 +65,8 @@ public class OrderServiceImplTest {
     }
 
     @Test
-    void getOrderById_shouldReturnEmptyOptionalNotExist(){
-        Long  orderId = 21L;
+    void getOrderById_shouldReturnEmptyOptionalNotExist() {
+        Long orderId = 21L;
         when(orderRepository.findById(orderId)).thenReturn(Optional.empty());
         Optional<Order> result = orderService.getOrderById(orderId);
         assertNotNull(result);
@@ -48,7 +74,9 @@ public class OrderServiceImplTest {
         assertTrue(result.isEmpty());
         verify(orderRepository, times(1)).findById(orderId);
         verifyNoMoreInteractions(orderRepository);
-    }  @Test
+    }
+
+    @Test
     void getAllOrders_returnListOfOrders() {
         // Given
         Order order1 = new Order(10L, "Item A", 1);
@@ -74,5 +102,57 @@ public class OrderServiceImplTest {
         verify(orderRepository, times(1)).findAll();
         verifyNoMoreInteractions(orderRepository);
     }
+
+    @Test
+    void createOrder_saveAndSendToKafka_whenOrderDoesNotExist() throws ExecutionException, InterruptedException, JsonProcessingException, ExecutionException {
+        Long orderId = 1L;
+        String item = "Dell Laptop";
+        Integer quantity = 1;
+        OrderRequestDTO orderRequestDTO = new OrderRequestDTO();
+        orderRequestDTO.setOrderId(orderId);
+        orderRequestDTO.setItem(item);
+        orderRequestDTO.setQuantity(quantity);
+
+        Order orderToSave = new Order();
+        orderToSave.setOrderId(orderId);
+        orderToSave.setItem(item);
+        orderToSave.setQuantity(quantity);
+
+        Order savedOrder = new Order(orderId, item, quantity);
+        String simulatedOrderJson = testObjectMapper.writeValueAsString(savedOrder);
+        when(orderRepository.existsById(orderId)).thenReturn(false);
+        when(orderRepository.saveAndFlush(any(Order.class))).thenReturn(savedOrder);
+        Order createdOrder = orderService.createOrder(orderRequestDTO);
+        assertNotNull(createdOrder);
+        assertEquals(orderId, createdOrder.getOrderId());
+        assertEquals(item, createdOrder.getItem());
+        assertEquals(quantity, createdOrder.getQuantity());
+
+        verify(orderRepository, times(1)).existsById(orderId);
+        verify(orderRepository, times(1)).saveAndFlush(any(Order.class));
+       verify(kafkaProducer, times(1)).send(eq(orderPushTopic), eq(orderId.toString()), anyString());
+        verifyNoMoreInteractions(orderRepository);
+        verifyNoMoreInteractions(kafkaProducer);
+    }
+    @Test
+    void createOrder_shouldThrowOrderAlreadyExistsException_whenOrderExists() throws ExecutionException, InterruptedException {
+        Long orderId = 1L;
+        OrderRequestDTO orderRequestDTO = new OrderRequestDTO();
+        orderRequestDTO.setOrderId(orderId);
+        when(orderRepository.existsById(orderId)).thenReturn(true);
+        when(messageSource.getMessage(eq("order.already.exists"), eq(new Object[]{orderId}), eq(Locale.ENGLISH))).thenReturn("Simulated Localized Message: Order already exists");
+        OrderAlreadyExistsException thrown = assertThrows(OrderAlreadyExistsException.class, () -> {
+            orderService.createOrder(orderRequestDTO);
+        }, "Should throw OrderAlreadyExistsException when order with ID exists");
+        assertEquals("Simulated Localized Message: Order already exists", thrown.getMessage());
+        verify(orderRepository, times(1)).existsById(orderId);
+        verify(messageSource    , times(1)).getMessage(eq("order.already.exists"), eq(new Object[]{orderId}), eq(Locale.ENGLISH));
+        verify(orderRepository, never()).saveAndFlush(any(Order.class));
+        verify(kafkaProducer, never()).send(anyString(), anyString(), anyString());
+        verifyNoMoreInteractions(orderRepository);
+        verifyNoMoreInteractions(messageSource);
+        verifyNoInteractions(kafkaProducer);
+    }
+
 
 }
